e5b781325d601dddebbb01262e2a683eef6a5081
Bilalmohamedali <bilalmohamedalipro@gmail.com>
2023-01-11 19:47:23 +0100

    Practicando pruebas unitarias con JUnit y Mockito

---
+++ src/test/java/TestServicioA.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import services.ServicioA;
+import services.impl.ServicioAImpl;
+
+public class TestServicioA {
+
+    @Test
+    public void testSumar(){
+        int a = 2;
+        int b = 2;
+        ServicioA servicioA = new ServicioAImpl();
+        assertEquals(servicioA.sumar(2,b), 4);
+
+    }
+
+}
